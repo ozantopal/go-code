c5400ddc9f99f519eab882d4817b2faa2a903579
Ozan TOPAL <ozantopal@msn.com>
2022-01-13 17:28:04 +0300

    Solve problem 104) maximum depth of binary tree'

---
+++ 104-maximum-depth-of-binary-tree/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	root := &TreeNode{
+		Val:  3,
+		Left: &TreeNode{Val: 9},
+		Right: &TreeNode{
+			Val:   20,
+			Left:  &TreeNode{Val: 15},
+			Right: &TreeNode{Val: 7},
+		},
+	}
+
+	fmt.Println(maxDepth(root))
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
