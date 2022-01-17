375939875698d6b3a92992df6a968118da545780
Ozan TOPAL <ozantopal@msn.com>
2022-01-17 20:32:28 +0300

    Solve problem '118) pascals triangle'

---
+++ 118-pascals-triangle/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(generate(5))
+	fmt.Println(generate(1))
+}
+
+func generate(numRows int) [][]int {
+	result := [][]int{}
+	for i := 0; i < numRows; i++ {
+		row := []int{}
+		for j := 0; j < i+1; j++ {
+			if j == 0 || j == i {
+				row = append(row, 1)
+			} else if i > 1 {
+				row = append(row, result[i-1][j-1]+result[i-1][j])
+			}
+		}
+
+		result = append(result, row)
+	}
+
+	return result
+}
