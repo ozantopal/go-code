56321b66ddc42f64b9b0405e30e9ea8668246cc7
Ozan TOPAL <ozantopal@msn.com>
2022-01-18 12:35:48 +0300

    Solve problem '136) single number'

---
+++ 136-single-number/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(singleNumber([]int{2, 2, 1}))
+	fmt.Println(singleNumber([]int{4, 1, 2, 1, 2}))
+	fmt.Println(singleNumber([]int{1}))
+}
+
+func singleNumber(nums []int) int {
+	res := 0
+	for _, num := range nums {
+		res ^= num
+	}
+
+	return res
+}
