97b871958821d002fe984c686f3fe0927d162af1
Ozan TOPAL <ozantopal@msn.com>
2022-01-20 15:32:15 +0300

    Solve problem '167) two sum ii - input array is sorted'

---
+++ problems/167-two-sum-ii-input-array-is-sorted/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{2, 3, 4}, 6))
+	fmt.Println(twoSum([]int{-1, 0}, -1))
+}
+
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for i < j {
+		if numbers[i]+numbers[j] == target {
+			return []int{i + 1, j + 1}
+		} else if numbers[i]+numbers[j] < target {
+			i++
+		} else {
+			j--
+		}
+	}
+
+	return nil
+}
